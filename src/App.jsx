3b79b24724f7feb4e767fd93c03086855a3d2ef0
Saif <saif49032@gmail.com>
2024-01-06 13:26:35 +0530

    Home-page/Auth-page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Route,Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage/HomePage'
+import AuthPage from './pages/AuthPage/AuthPage'
 
 const App = () => {
   return (
-    <div>App</div>
+<>
+<Routes>
+  <Route path='/' element={<HomePage/>}/>
+  <Route path='/auth' element={<AuthPage/>}/>
+
+</Routes>
+</>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,28 @@ import App from "./App.jsx";
 import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import { extendTheme } from '@chakra-ui/react'
+import {mode} from '@chakra-ui/theme-tools'
+import {BrowserRouter} from "react-router-dom"
+const styles={
+  global: (props) => ({
+    body: {
+      color: mode('gray.800', 'whiteAlpha.900')(props),
+      bg: mode('gray.100', '#000')(props),
+    },
+  }),
+}
 
 const config = {
   initialColorMode: 'dark',
   useSystemColorMode: false,
 }
-const theme = extendTheme({ config })
+const theme = extendTheme({ config ,styles})
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <BrowserRouter>
     <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
