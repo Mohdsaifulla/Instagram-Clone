68d54d715c7cb731304ada2600ebbc2dadfbb3b4
Saif <saif49032@gmail.com>
2024-01-09 15:05:56 +0530

    side bar updated

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,9 +1,74 @@
-import React from 'react'
-
+import { Avatar, Box, Flex, Link } from "@chakra-ui/react";
+import React from "react";
+import { Link as RouterLink } from "react-router-dom";
+import { AiFillHome } from "react-icons/ai";
+import {
+  CreatePostLogo,
+  InstagramLogo,
+  InstagramMobileLogo,
+  NotificationsLogo,
+  SearchLogo,
+} from "../../assets/constants";
 const Sidebar = () => {
+  const sidebarItems = [
+    {
+      icon: <AiFillHome size={25} />,
+      text: "Home",
+      link: "/",
+    },
+    {
+      icon: <SearchLogo />,
+      text: "Search",
+    },
+    {
+      icon: <NotificationsLogo />,
+      text: "Notifications",
+    },
+    {
+      icon: <CreatePostLogo />,
+      text: "Create",
+    },
+    {
+      icon: <Avatar name='Dan Abrahmov' src='https://bit.ly/dan-abramov'/>,
+      text: "Profile",
+      link: "/",
+    },
+  ];
   return (
-    <div>Sidebar</div>
-  )
-}
+    <Box
+      h={"100vh"}
+      borderRight={"1px solid"}
+      borderColor={"whiteAlpha.300"}
+      py={8}
+      position={"sticky"}
+      top={0}
+      left={0}
+      px={{ base: 2, md: 4 }}
+    >
+      <Flex direction={"column"} gap={10} w={"full"} h={"full"}>
+        <Link
+          to={"/"}
+          as={RouterLink}
+          pl={2}
+          display={{ base: "none", md: "block" }}
+        >
+          <InstagramLogo />
+        </Link>
+        <Link
+          to={"/"}
+          as={RouterLink}
+          pl={2}
+          display={{ base: "block", md: "none" }}
+          borderRadius={6}
+          _hover={{ bg: "whiteAlpha.200" }}
+          w={10}
+          cursor={"pointer"}
+        >
+          <InstagramMobileLogo />
+        </Link>
+      </Flex>
+    </Box>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
