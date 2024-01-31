042bbff11687a62ba292c057de3bf1349e11886e
Saif <saif49032@gmail.com>
2024-01-31 15:00:36 +0530

    feat added

---
+++ src/hooks/useGetUserProfileByUsername.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import useShowToast from "./useShowToast";
+import useUserProfileStore from "../store/userProfileStore";
+import {
+  QuerySnapshot,
+  collection,
+  doc,
+  getDocs,
+  query,
+  where,
+} from "firebase/firestore";
+import { firestore } from "../firebase/firebase";
+
+
+const useGetUserProfileByUsername = (username) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const showToast = useShowToast();
+  const { userProfile, setUserProfile } = useUserProfileStore();
+  // const userProfile=useUserProfileStore(state =>state.userProfile)
+// console.log(username)
+  useEffect(() => {
+    const getUserProfile = async () => {
+      setIsLoading(true);
+      try {
+        const q = query(
+          collection(firestore, "users"),
+          where("username", "==", username)
+        );
+        const querySnapshot = await getDocs(q);
+        if (QuerySnapshot.empty) return setUserProfile(null);
+        let userDoc;
+        querySnapshot.forEach((doc) => {
+          userDoc = doc.data();
+        });
+        setUserProfile(userDoc);
+        console.log(userDoc)
+      } catch (error) {
+        showToast("Error", error.message, "error");
+      }finally{
+        setIsLoading(false)
+      }
+    };
+    getUserProfile();
+  }, [setUserProfile, username, showToast]);
+  return {isLoading,userProfile}
+};
+
+export default useGetUserProfileByUsername;
--- src/hooks/useShowToast.js
+++ src/hooks/useShowToast.js
@@ -1,9 +1,9 @@
 import { useToast } from '@chakra-ui/react'
-import React from 'react'
+import React, { useCallback } from 'react'
 
 const useShowToast = () => {
     const toast = useToast()
- const showToast=(title,description,status)=>{
+ const showToast=useCallback((title,description,status)=>{
     toast({
         title:title,
         description:description,
@@ -11,7 +11,7 @@ const useShowToast = () => {
         duration:3000,
         isClosable:true,
     })
- }
+ },[toast])
 
     return showToast
 }
